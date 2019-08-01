506329c5add53759416139b56e3776a499a0d320
compscicardwell <compscicardwell@gmail.com>
2019-08-01 16:19:34 -0400

    first commit

---
+++ src/main/java/com/mindtree/AnytimeLibrary/AnytimeLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.mindtree.AnytimeLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnytimeLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnytimeLibraryApplication.class, args);
+	}
+
+}
